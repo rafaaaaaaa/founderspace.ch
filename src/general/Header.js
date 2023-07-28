e0ed7472a42237d9f5074d2115d5ffc1f8adfc7d
Rafael Estermann <33682680+rafaaaaaaa@users.noreply.github.com>
2023-07-28 14:18:54 +0200

    added peekaboo to header (but sadly removed full blurry background

--- src/general/Header.js
+++ src/general/Header.js
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react";
 import Link from "./Link";
 import Hamburger from "hamburger-react";
 import Button from "./Button";
+import { useScrollDirection } from "../components/useScrollDirection";
 
 function Header() {
   const [opened, setOpened] = useState(false);
   const [width, setWidth] = useState(window.innerWidth);
+  const scrollDirection = useScrollDirection();
 
   function handleWindowSizeChange() {
     setWidth(window.innerWidth);
@@ -20,36 +22,48 @@ function Header() {
   const isMobile = width <= 768;
 
   return (
-    <header>
-      <div>
-        {opened && isMobile && (
-          <div
-            id="blurOverlay"
-            className="fixed top-0 left-0 w-full h-full backdrop-blur-sm bg-transparent z-30 none"
-          />
-        )}
-        <nav className="backdrop-blur-sm fixed top-0 left-0 z-50 w-full">
-          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="/" className="flex items-center">
+    <header className={`sticky ${scrollDirection === "down" && !opened ? "-top-24" : "top-0"} h-24 transition-all duration-500 z-50`}>
+
+      <nav className="backdrop-blur-sm">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a href="/" className="flex items-center">
+            <img
+              src={require("../founderspace-logo.png")}
+              className="h-16 mr-3"
+              alt="FounderSpace Logo"
+            />
+            {opened && isMobile && (
               <img
                 src={require("../founderspace-logo.png")}
-                className="h-16 mr-3"
+                className="h-16 mr-3 absolute logo-glow"
                 alt="FounderSpace Logo"
               />
-              {opened && isMobile && (
-                <img
-                  src={require("../founderspace-logo.png")}
-                  className="h-16 mr-3 absolute logo-glow"
-                  alt="FounderSpace Logo"
-                />
-              )}
-            </a>
-            <div className="inline-flex md:hidden">
-              <Hamburger toggled={opened} toggle={setOpened} color="white" />
-            </div>
+            )}
+          </a>
+          <div className="inline-flex md:hidden">
+            <Hamburger toggled={opened} toggle={setOpened} color="white" />
+          </div>
 
-            <div className="hidden w-full md:block md:w-auto z-50">
-              <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border md:flex-row md:space-x-8 md:mt-0 md:border-0 items-center">
+          <div className="hidden w-full md:block md:w-auto z-50">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border md:flex-row md:space-x-8 md:mt-0 md:border-0 items-center">
+              <li>
+                <Link href="events" title="Events" />
+              </li>
+              <li>
+                <Link href="partnerships" title="Partnerships" />
+              </li>
+              <li>
+                <Link href="board" title="Board" />
+              </li>
+              <li>
+                <Button href="join" text="Join Us" />
+              </li>
+            </ul>
+          </div>
+
+          {opened && (
+            <div className="block w-full md:hidden">
+              <ul className="font-medium flex flex-col pt-4 gap-4 text-2xl">
                 <li>
                   <Link href="events" title="Events" />
                 </li>
@@ -60,32 +74,13 @@ function Header() {
                   <Link href="board" title="Board" />
                 </li>
                 <li>
-                  <Button href="join" text="Join Us" />
+                  <Link href="join" title="Join Us" />
                 </li>
               </ul>
             </div>
-
-            {opened && (
-              <div className="block w-full md:hidden">
-                <ul className="font-medium flex flex-col pt-4 gap-4 text-2xl">
-                  <li>
-                    <Link href="events" title="Events" />
-                  </li>
-                  <li>
-                    <Link href="partnerships" title="Partnerships" />
-                  </li>
-                  <li>
-                    <Link href="board" title="Board" />
-                  </li>
-                  <li>
-                    <Link href="join" title="Join Us" />
-                  </li>
-                </ul>
-              </div>
-            )}
-          </div>
-        </nav>
-      </div>
+          )}
+        </div>
+      </nav>
     </header>
   );
 }
